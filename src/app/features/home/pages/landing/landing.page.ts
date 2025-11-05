54af70ff361ec0b7cfb6a2ec0d8a8427e28a7099
Phuong bui bui <phuong.nguyenthanh90@gmail.com>
2025-11-05 21:37:44 +0700

    update

--- src/app/features/home/pages/landing/landing.page.ts
+++ src/app/features/home/pages/landing/landing.page.ts
@@ -1,46 +1,27 @@
-import {
-  Component,
-  OnInit,
-  AfterViewInit,
-  ViewChild,
-  ElementRef,
-} from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { register } from 'swiper/element/bundle';
 import { Router } from '@angular/router';
-register();
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.page.html',
   styleUrls: ['./landing.page.scss'],
   standalone: true,
   imports: [IonContent, CommonModule, FormsModule, TranslateModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class LandingPage implements OnInit, AfterViewInit {
-  @ViewChild('swiperEl', { static: true }) swiperEl?: ElementRef<any>;
-
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    register();
+  }
 
   ngOnInit() {}
 
-  ngAfterViewInit(): void {
-    const el = this.swiperEl?.nativeElement as any;
-    if (!el) return;
-
-    Object.assign(el, {
-      slidesPerView: 1,
-      loop: true,
-      autoplay: { delay: 3000, disableOnInteraction: false },
-      pagination: { clickable: true },
-    });
-
-    el.initialize();
-  }
+  ngAfterViewInit(): void {}
 
   start(): void {
     this.router.navigateByUrl('/login');
