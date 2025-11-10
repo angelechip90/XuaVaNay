97dbfdf1468d23d2be5a07337762b58b66062adb
Phuong bui bui <phuong.nguyenthanh90@gmail.com>
2025-11-10 11:47:10 +0700

    update android connection

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,29 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ai.xuavanay.app',
   appName: 'Xưa và Nay',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
+    CapacitorHttp:{
+      enabled:true
+    },
+    SplashScreen:{
+      launchAutoHide:true,
+      launchShowDuration:2000,
+      splashFullScreen:true,
+      splashImmersive:true,
+    },
+    Badge: {
+      persist: false,
+      autoClear: false,
+    },
+  },
+  server: {
+    androidScheme: 'https',
+    cleartext: true,
+  }
 };
 
 export default config;
